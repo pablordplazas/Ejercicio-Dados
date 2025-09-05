d38745be64e9ef4595c0812ef196ad50adaa5897
pablordplazas <pablordplazas@gmail.com>
2025-09-05 00:07:31 -0500

    ejercicio terminado

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,34 @@
 import java.util.Random;
-
 public class Main {
     public static void main(String[] args) {
-        int i = 0;
-        int Da1, Da2, SumaDa1 = 0, SumaDa2 = 0;
-        for ( i = 0; i <= 20; i++)  {
+        var i = 0;
+        int Da1, Da2, Da3, SumaDa1 = 0, SumaDa2 =0, SumaDa3 = 0;
+        for (i = 0; i <= 20; i++) {
 
             Random aleatorio = new Random();
-            Da1 = aleatorio.nextInt((6-1)+1)+1;
-            Da2 = aleatorio.nextInt((6-1)+1)+1;
+            Da1 = aleatorio.nextInt((6 - 1) + 1) + 1;
+            Da2 = aleatorio.nextInt((6 - 1) + 1) + 1;
+            Da3 = aleatorio.nextInt((6 - 1) + 1) + 1;
+            SumaDa3 += Da3;
             SumaDa1 += Da1;
             SumaDa2 += Da2;
 
 
         }
-        if (SumaDa1>SumaDa2) {
-            System.out.println("La suma total de el dado #1 es mayor que el #2" +SumaDa1);
-
+        if (SumaDa1 > SumaDa2 && SumaDa1 > SumaDa3) {
+            System.out.println("El Dado #1 GANA es mayor que Dado #2 y #3: " + SumaDa1);
+            System.out.println("El valor de #2 es: " + SumaDa2);
+            System.out.println("El valor de #3 es: " + SumaDa3);
+        } else if (SumaDa2 > SumaDa1 && SumaDa2 > SumaDa3) {
+            System.out.println("El Dado #2 GANA es mayor que el #1 y #3: " + SumaDa2);
+            System.out.println("El valor de #1 es: " + SumaDa1);
+            System.out.println("El valor de #3 es: " + SumaDa3);
+        } else if (SumaDa3 > SumaDa1 && SumaDa3 > SumaDa2) {
+            System.out.println("El Dado #3 GANA es mayor que el #1 y #2: " + SumaDa3);
+            System.out.println("El valor de #1 es: " + SumaDa1);
+            System.out.println("El valor de #2 es: " + SumaDa2);
         } else {
-            System.out.println("La suma total de el dado #2 es mayor que el #1" +SumaDa2);
-
+            System.out.println("Hay un empate entre los dados.");
         }
     }
 }
